96ae68b8d1e2c42df9c1fb635cfac06d096a766a
thy <1756127061@qq.com>
2023-07-21 01:13:13 +0800

    chore: change oidc domain

--- config.ts
+++ config.ts
@@ -5,7 +5,7 @@ const API_ROOT = "https://api.cc98.org";
 
 const OPENID_ROOT = "https://openid.cc98.org";
 
-const CURRENT_ROOT = "https://cc98-summary.pages.dev/";
+const CURRENT_ROOT = "https://cc98-summary.vercel.app/";
 
 const TOPIC_PER_REQUEST = 20; // 一次请求的主题数量
 
