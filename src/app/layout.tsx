29e4178be4b9bf12b841ac7d62ecaf36ebcb9c78
thy <1756127061@qq.com>
2024-09-08 11:29:06 +0800

    perf: speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,8 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-        <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}</body>
       <Analytics />
+      <SpeedInsights />
     </html>
   )
 }
